3ae5ae3cf6a3d76d85253bea0260c225ba935f61
Erika <brandonerika96@gmail.com>
2024-04-11 21:02:40 -0400

    problem 11 to be continued

--- problems/11-flatten.js
+++ problems/11-flatten.js
@@ -11,11 +11,32 @@ flatten([]); // []
 flatten([1, 2]); // [1, 2]
 flatten([1, [2, [3]]]); // [1, 2, 3]
 ***********************************************************************/
+//assign a variable to arr.pop()
+//obtain the array and test to see if it's an array (Array.isArray)
+//if true, then call the recursive function flatten(arr.length - 1)
+
+
 
 function flatten(arr) {
-  // Your code here 
+  if (arr.length === 0) {
+    return [];
+  } else {
+    let emptyArr = []
+    let testingArr = arr.pop() //extracts the 'first' element
+    if (Array.isArray(testingArr)) {
+      return emptyArr = emptyArr.concat(flatten(testingArr));
+      //console.log(testingArr.concat(emptyArr))
+      //console.log(flatten(testingArr))
+     } else {
+      emptyArr.push(testingArr);
+    }
+    return emptyArr
+    //return emptyArr.concat(flatten(arr));
+  }
 }
-  
+
+
+
+console.log(flatten([1, [2, [3]]])); // [1, 2, 3])
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 module.exports = flatten;
-  
